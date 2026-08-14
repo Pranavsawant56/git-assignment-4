eea07e0d90ed0b577ef9efdbd7707bea432d8232
pranav sawant <sawantpranav773@gmail.com>
2026-08-14 12:04:19 +0530

    Add To-Do backend API

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+import pymongo
 import json
+
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = pymongo.MongoClient(MONGO_URI)
+collection = db["mycollection"]
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.todo('/todo')
+def todo():
+    return render_template('to-do.html')
+
 
 @app.route('/api', methods=['POST'])
 def api():
@@ -30,6 +44,15 @@ def api():
 
     except Exception as e:
         return f"❌ Error: {e}"
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    try:
+        form_data =dict(request.form)
+        collection.insert_one(form_data)
+        return:"Todo submited successfully "
+    except Exception as e :
+        return f" Error:{e}"
+   
 
 
 if __name__ == '__main__':
